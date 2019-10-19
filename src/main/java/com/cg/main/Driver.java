862afe187501437c32b8408f7c7ccff92fc37711
SSINHA23 <SSINHA23@DIN51002654.corp.capgemini.com>
2019-10-19 17:35:53 +0530

    This is first commit

---
+++ src/main/java/com/cg/main/Driver.java
@@ -0,0 +1,9 @@
+package com.cg.main;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println("Hi I am main");
+	}
+
+}
---
+++ src/main/java/com/cg/ui/Hello.java
@@ -0,0 +1,10 @@
+package com.cg.ui;
+
+public class Hello {
+
+	
+	public void display() {
+		System.out.println("Hi this is before commit");
+	}
+	
+}
